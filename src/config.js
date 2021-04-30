7c0da31b9435cf471666a6587baf530db9de7d30
Christopher 'ZK' Hal <zkzerotwo@gmail.com>
2021-04-30 15:34:54 -0500

    cors endpoint check

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export default {
     SEARCH_ENDPOINT: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search",
     API_ENDPOINT_SAVE: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes",
     // AUTH_ENDPOINT: "http://localhost:8000/api",
-    AUTH_ENDPOINT: "https://vinary-capstone-server.herokuapp.com/api",
+    AUTH_ENDPOINT: "https://vinary-capstone-server.herokuapp.com",
     SEARCH_TOKEN: "d36a838384mshc96686c738af0aap11fd78jsnf972d810e8ef",
     APP_API_TOKEN: "5aa839d1-25be-43cf-b796-57681bd1184c"
 }
